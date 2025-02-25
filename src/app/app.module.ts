1de6a0447cca46cc255527d98c7df5401da7abd5
kenjidelmundo <kenji04dmundo@gmail.com>
2025-02-25 14:46:17 +0800

    server.js is final what i do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { HeaderComponent} from './header/header.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.module';
+import { PostsService } from '../post.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+    posts: Post[] = [];
+    private postsSub!: Subscription;
+    constructor(public postsService: PostsService){
+    }
+    ngOnInit() {
+        this.posts = this.postsService.getPosts();
+        this.postsSub = this.postsService.getPostUpdatedListener()
+        .subscribe((posts: Post[])=>{
+            this.posts = posts;
+        });
+    }
+    ngOnDestroy() {
+        this.postsSub.unsubscribe();
+    }
+}
